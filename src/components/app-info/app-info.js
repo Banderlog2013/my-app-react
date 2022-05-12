0db50abb783cc40ebd23de747d4295397152881b
Pavel <banderlog2013@yandex.ru>
2022-05-12 09:49:46 +0300

    app_react

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников в компании Novkros</h1>
+            <h2>общее число сотруников: </h2>
+            <h2>Премию получат: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
