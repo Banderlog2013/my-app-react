838c2a7448646b0e5e46cf5471a15a4c331bebb8
Pavel <banderlog2013@yandex.ru>
2022-05-22 22:06:09 +0300

    immutabelnost

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,26 +8,60 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'Вася Пупкин', salary: 800, increase: false, id: 1},
+                {name: 'Пупок Васин', salary: 13000, increase: true, id: 2},
+                {name: 'Петя Камушкин', salary: 5000, increase: false, id: 3}
+            ] 
+        }
+        this.maxId = 4; // устанвавливаем id от 4
+    }
 
-    const data = [
-        {name: 'Вася Пупкин', salary: 800, increase: false, id: 1},
-        {name: 'Пупок Васин', salary: 13000, increase: true, id: 2},
-        {name: 'Петя Камушкин', salary: 5000, increase: false, id: 3},
-    ] 
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
 
-    return (
-        <div className="app">
-            <AppInfo/>
+    addItem = (name, salary) => { // создаем метод с аргументами
+        const newItem = { // создаем новую переменную в виде объекта
+            name, 
+            salary,
+            increase: false, // инкрис устанавливаем в фолс
+            id: this.maxId++ // присваиваем id на единицу больше
+        }
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+        this.setState(({data}) => { // создаем состояние
+            const newArr = [...data, newItem]; // новый массив
+            return {
+                data: newArr // возращаем новый массив
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                <EmployeesList 
+                data={this.state.data}
+                onDelete={this.deleteItem}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
-            <EmployeesList data={data}/>
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
+
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,23 +1,49 @@
+import { Component } from 'react';
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="З/П в $?" />
+class EmployeesAddForm extends Component {
+    constructor(props) { // конструктор со свойствами
+        super(props); // вызываем супер со свойствами
+        this.state = { // создаем состояние (объект)
+            name: '',
+            salary: ''
+        }
+    }
 
-                <button type="submit"
-                        className="btn btn-outline-light">Добавить</button>
-            </form>
-        </div>
-    )
+    onValueChange = (e) => { // создаем метод, который принимает событие
+        this.setState({ // меняем сотсояние
+            [e.target.name] : e.target.value // принимаем событие
+        })
+
+    }
+
+    render () {
+        const {name, salary} = this.state; // создаем переменные и вытаскиваем их из state
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form
+                    className="add-form d-flex">
+                    <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="Как его зовут?" 
+                        name="name" /* назначаем атрибут для принятия сотояния */
+                        value={name}
+                        onChange={this.onValueChange}/> {/* обработчик событий с методом */}
+                    <input type="number"
+                        className="form-control new-post-label"
+                        placeholder="З/П в $?"
+                        name="salary"
+                        value={salary}
+                        onChange={this.onValueChange}/> {/* обработчик событий с методом */}
+    
+                    <button type="submit"
+                            className="btn btn-outline-light">Добавить</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default EmployeesAddForm;
+
